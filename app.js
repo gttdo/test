6095fabb6e1a71d3da6ff1f7e863487fcd492a5c
gttdo <vinces.gerardo@gmail.com>
2019-10-27 19:32:04 -0700

    test

---
+++ app.js
@@ -0,0 +1,15 @@
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser")
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.set("view engine", "ejs");
+    app.use(express.static(__dirname + "/views"));
+
+    app.get("/", function(req, res){
+            res.render("index");
+    });
+
+    app.listen(3000, function(){
+      console.log("Server has started!");
+    });
